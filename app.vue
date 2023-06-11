04f94b010a05200fffe2f28c9020a629459e95b2
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-06-11 14:09:24 +0700

    point 1: Tambahkan Empty State dimana akan muncul saat tasks kosong

--- app.vue
+++ app.vue
@@ -1,29 +1,39 @@
 <template>
-  <div class="list-task" >
-    
-    <div class="item-task d-flex align-items-start border-bottom pt-3 pb-4">
-      <input
-        id="task"
-        type="checkbox"
-        name="status"
-        class="me-2 mt-2"
-        :checked="tasks[0].isDone"
-      />
-      <div class="d-flex flex-column">
-        <div class="title-task mb-1">
-          {{ tasks[0].title }}
-        </div>
-        <div class="description-task small text-muted">
-          {{ tasks[0].description }}
+  <div class="list-task">
+    <header>
+      <h1 class="text-3xl font-bold">Todo List</h1>
+    </header>
+    <div v-show="isEmpty">
+      <p class="font-semibold">Belum Ada Task</p>
+    </div>
+    <div v-for="(task, index) in tasks" :key="index">
+      <div
+        class="item-task d-flex align-items-start border-bottom pt-3 pb-4"
+        v-bind="key"
+      >
+        <input
+          id="task"
+          type="checkbox"
+          name="status"
+          class="me-2 mt-2"
+          :checked="task.isDone"
+        />
+        <div class="d-flex flex-column">
+          <div class="title-task mb-1">
+            {{ task.title }}
+          </div>
+          <div class="description-task small text-muted">
+            {{ task.description }}
+          </div>
         </div>
       </div>
     </div>
-      
+
     <div class="action py-2">
       <a
-        v-if="isCreating"
+        v-if="!isCreating"
         href="#"
-        class="add-button"
+        class="underline text-blue-500 cursor-pointer"
         @click="isCreating = !isCreating"
         >Add Task</a
       >
@@ -31,11 +41,13 @@
         <div class="card mb-2">
           <div class="card-body d-flex flex-column p-0">
             <input
+              v-model="titleValue"
               class="form-control border-0 mb-2"
               placeholder="Title"
               type="text"
             />
             <textarea
+              v-model="descriptionValue"
               class="form-control border-0 small"
               placeholder="Description"
               rows="3"
@@ -62,30 +74,37 @@ export default {
     return {
       // Daftar task
       tasks: [
-        {
-          title: "Task 1",
-          description: "ini deskripsi",
-          isDone: false,
-        },
+        // {
+        //   title: "Task 1",
+        //   description: "ini deskripsi",
+        //   isDone: false,
+        // },
       ],
       isCreating: false,
       // state for handle form input
       titleValue: ``,
-      descriptionValue: ``
+      descriptionValue: ``,
+      isEmpty: true,
     };
   },
-  methods : {
+  mounted() {
+    const tasks = this.tasks.length === 0;
+    !tasks ? (this.isEmpty = false) : (this.tasks = true);
+  },
+  methods: {
     addTask() {
-      this.tasks.push({
-        title: this.titleValue ,
-        description : this.descriptionValue ,
-        isDone : false
-      })
+      // console.log(this.titleValue);
+      const data = {
+        title: this.titleValue,
+        description: this.descriptionValue,
+        isDone: false,
+      };
+      this.tasks.push(data);
 
       // clear the form input state
-      this.titleValue = ``
-      this.descriptionValue = ``
-    }
-  }
+      this.titleValue = ``;
+      this.descriptionValue = ``;
+    },
+  },
 };
 </script>
